3b1ea50f62629b92545905a8c42a61bd901ae017
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2022-11-30 22:35:52 +0100

    Update class.py

--- class.py
+++ class.py
@@ -1,1 +1,24 @@
+class Animal():
+  def __init__(self):
+    self.name = name
+    self.legs = legs
+    self.owner = owner
+   
+  def speak(sound):
+    print(sound)
 
+
+    
+class Dog():
+  
+  def __init__(self):
+    self.name = name
+    self.legs = legs
+    self.owner = owner
+   
+  def speak(sound):
+    print(sound)
+    
+  def fetch(item):
+    print("I fetched" + str(item))
+  

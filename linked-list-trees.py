8b3c0d32fc401b6e003c1fd1e78e5b7e06c9fb66
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2022-12-01 22:44:54 +0100

    Update linked-list-trees.py

--- linked-list-trees.py
+++ linked-list-trees.py
@@ -1,1 +1,10 @@
-
+class Link:
+  empty = ()
+  
+  det __init__(self, first, rest=empty):
+    assert self.rest is Link.empty or isinstance(self.rest, Link) 
+    self.first = first
+    self.rest = rest
+    
+    
+    
